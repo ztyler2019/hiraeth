1e7300b9a66721183cb2c5c62954695a1eaee40f
ztyler2019 <77475329+ztyler2019@users.noreply.github.com>
2022-01-25 13:56:56 -0800

    venmo

--- src/Components/Payments.js
+++ src/Components/Payments.js
@@ -2,7 +2,10 @@
 
 function Payment() {
     return (
+      <div>
       <a href="https://cash.app/$SpaceKowBoi/5">Cash App</a>
+      <a href="venmo://paycharge?txn=pay&recipients=Zack-Tyler-19&amount=5&note=Donation%20for%20future%20Cubic%20Collective%20projects">Venmo</a>
+      </div>
     );
   }
 
