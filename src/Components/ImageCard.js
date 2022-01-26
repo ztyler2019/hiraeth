22acb2cbf6a6e58271b4d18523a60ba05f1aaa22
ztyler2019 <77475329+ztyler2019@users.noreply.github.com>
2022-01-25 22:23:38 -0800

    mobile working minus footer

--- src/Components/ImageCard.js
+++ src/Components/ImageCard.js
@@ -4,13 +4,17 @@ import frame from '../Images/frame.png';
 
 function App() {
   return (
-    <div className="image-card image-card-fade">
-        <div className='nft-box'>
-          <div className='frame'>
-          <img src={frame} alt='frame'/>
+    <div className="image-card">
+      <div className='container'>
+      <div className='row'>
+          <div className='col-xl-4 col-lg-6 col-md-12'>
+            <img src={frame} alt='frame' className='frame'/>
           </div>
-          <ETHpayment />
-        </div>
+          <div className='col-xl-8 col-lg-6 col-md-12'>
+            <ETHpayment />
+          </div>
+      </div>
+      </div>
       
       
         
