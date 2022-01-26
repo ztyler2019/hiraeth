9985d6c1c03ae9a9fa573b40b2b4c8180dbcb210
ztyler2019 <77475329+ztyler2019@users.noreply.github.com>
2022-01-25 20:53:06 -0800

    payment links working

--- src/Components/ETHpayment.js
+++ src/Components/ETHpayment.js
@@ -2,7 +2,10 @@ import { useState } from "react";
 import { ethers } from "ethers";
 import ErrorMessage from "../ErrorMessage";
 import TxList from "../TxList";
-import PaymentButton from './Payments';
+
+import { ReactComponent as CashApp } from '../Images/cash-app-seeklogo.com.svg';
+import { ReactComponent as Venmo } from '../Images/venmo-icon.svg';
+import { ReactComponent as Ethereum } from '../Images/ethereum.svg';
 
 //import '../css/ETHcard.css'
 import '../css/ETH.css' 
@@ -37,13 +40,22 @@ export default function App() {
     e.preventDefault();
     const data = new FormData(e.target);
     setError();
+    if (paymentType === "Ethereum") {
     await startPayment({
       setError,
       setTxs,
       ether: data.get("ether"),
       addr: data.get("addr")
     });
+    } else if (paymentType === "CashApp") {
+      window.location.href = "https://cash.app/$SpaceKowBoi/5";
+    } else if (paymentType === "Venmo") {
+      window.location.href = "venmo://paycharge?txn=pay&recipients=Zack-Tyler-19&amount=5&note=Donation%20for%20future%20Cubic%20Collective%20projects";
+    }
+    
   };
+
+  const [paymentType, setPaymentType] = useState("");
 
   return (
     <form onSubmit={handleSubmit}>
@@ -53,13 +65,51 @@ export default function App() {
             Help Fund Future Projects
           </h1>
           <div>
-            <PaymentButton />
+
+          <div>
+        
+            <input 
+              type="radio" 
+              className="radio_item" 
+              value="CashApp" 
+              name="item" 
+              id="radio1"
+              onChange={(e) => setPaymentType("CashApp")}
+            />
+            <label className="label_item" htmlFor="radio1"> <CashApp/> </label>
+
+            <input 
+              type="radio" 
+              className="radio_item" 
+              value="Venmo" 
+              name="item" 
+              id="radio2"
+              onChange={(e) => setPaymentType("Venmo")}
+            />
+            <label className="label_item" htmlFor="radio2"> <Venmo/> </label>
+
+            <input 
+              type="radio" 
+              className="radio_item" 
+              value="Ethereum" 
+              name="item" 
+              id="radio3"
+              onChange={(e) => setPaymentType("Ethereum")}
+            />
+            <label className="label_item ethereum" htmlFor="radio3"> <Ethereum/> </label>
+
+            {/*<button className='payment-buttons'><CashApp/></button>
+
+              <a href="">Cash App</a>
+              <a href="">Venmo</a>*/}
+          </div>
+
             <div className="my-3 input">
               <input
                 name="ether"
                 type="text"
                 className="input input-bordered block w-full focus:ring focus:outline-none"
-                placeholder="Ethereum"
+                placeholder={paymentType}
               />
             </div>
             
@@ -70,7 +120,7 @@ export default function App() {
             type="submit"
             className="button"
           >
-            Thanks
+            Donate
           </button>
           <ErrorMessage message={error} />
           <TxList txs={txs} />
