c193efa11511308d4d42096ec328edb68495bace
ztyler2019 <77475329+ztyler2019@users.noreply.github.com>
2022-04-18 18:30:09 -0700

    made random social icon animations

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -25,11 +25,11 @@ function App() {
         </div>
     
         <div className='social-container col-lg-4 col-md-3 col-12' >
-            <a href="https://www.youtube.com/channel/UCqU0YrP58xBnBTk4XzeJTPw"><img className='social-media' src={yt} alt='YouTube'/></a>
-            <a href="https://www.patreon.com/hiraeth2022"><img className='social-media' src={patreon} alt='Patreon'/></a>
-            <a href="https://www.instagram.com/zacks_cozy_corner/"><img className='social-media' src={insta} alt='Instagram'/></a>
-            <a href="https://www.reddit.com/user/ZskrillaVkilla"><img className='social-media' src={reddit} alt='Reddit'/></a>
-            <a href="https://twitter.com/SpayceKowBoi"><img className='social-media' src={twitter} alt='Twitter'/></a>
+            <a href="https://www.youtube.com/channel/UCqU0YrP58xBnBTk4XzeJTPw"><img className='social-media1' src={yt} alt='YouTube'/></a>
+            <a href="https://www.patreon.com/hiraeth2022"><img className='social-media2' src={patreon} alt='Patreon'/></a>
+            <a href="https://www.instagram.com/zacks_cozy_corner/"><img className='social-media3' src={insta} alt='Instagram'/></a>
+            <a href="https://www.reddit.com/user/ZskrillaVkilla"><img className='social-media4' src={reddit} alt='Reddit'/></a>
+            <a href="https://twitter.com/SpayceKowBoi"><img className='social-media5' src={twitter} alt='Twitter'/></a>
           
         </div>
       </Container>
